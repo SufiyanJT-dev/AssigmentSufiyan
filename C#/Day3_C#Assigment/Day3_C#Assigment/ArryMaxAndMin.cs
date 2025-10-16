bc0c6bf014a0c2a7d0f676933f0f0929be8adffc
Sufiyan J T <sufiyan.jt@geekywolf.local>
2025-10-16 12:05:36 +0530

    Day3C#Assigment

---
+++ C#/Day3_C#Assigment/Day3_C#Assigment/ArryMaxAndMin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_C_Assigment
+{
+    public class ArryMaxAndMin
+    {
+        public ArryMaxAndMin() {
+            int [] arr;
+            int Length;
+            int Max;
+            int Min;
+            Console.WriteLine("Enter the total Numbers In array:");
+            String StringLenth=Console.ReadLine();
+            
+            if(int.TryParse(StringLenth, out Length))
+            {
+                arr = new int[Length];
+                Console.WriteLine("Enter the  Numbers to array:");
+                for (int i = 0; i < Length; i++)
+                {
+                    String Stringi = Console.ReadLine();
+                    if(int.TryParse (Stringi, out int num))
+                    {
+                        arr[i] = num;
+                    }
+                    else
+                    {
+                        arr[i] = 0;
+                    }
+                   
+                }
+                MinAndMax(arr, out Max, out Min);
+                Console.WriteLine("Max:"+Max+"Min:"+Min);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Lenth");
+            }
+             
+        }
+        public static void MinAndMax(int [] Arr,out int Max,out int Min)
+        {
+            Max=Arr[0];
+            Min=Arr[0];
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                if (Arr[i] > Max)
+                {
+                    Max = Arr[i]; 
+                }
+                if (Arr[i] < Min) { 
+                Min=Arr[i];
+                 }
+            }
+        }
+
+    }
+}
---
+++ C#/Day3_C#Assigment/Day3_C#Assigment/GetSumAndAverage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_C_Assigment
+{
+    public class GetSumAndAverage
+    {
+        //public GetSumAndAverage() {
+        //    Console.WriteLine("two numbers pls");
+        //    String StringNumber1=Console.ReadLine();
+        //    String StringNumber2=Console.ReadLine();
+        //    if(int.TryParse(StringNumber1,))
+        //}
+        
+        static GetSumAndAverage()
+        {
+            Console.WriteLine("enter two number");
+            string StringNumber1 = Console.ReadLine();
+            string StringNumber2 = Console.ReadLine();
+            if (int.TryParse(StringNumber1, out int Number1) && int.TryParse(StringNumber2, out int Number2))
+            {
+                double Sum=0;
+                double Avg;
+               
+                SumAndAvg(Number1,Number2,out Sum,out Avg);
+
+                Console.WriteLine("Sum:" + Sum + "Avg:" + Avg);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+       public static void SumAndAvg(int Number1,int  Number2, out double Sum, out double Avg)
+        {
+            Sum=0;
+            Avg=1;
+            Avg = (Number1 + Number2) / 2;
+            Sum = Number1 + Number2;
+        }
+    }
+}
