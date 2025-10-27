eb2176cfd54c0a0a6c5e3c786b3f98c3691027f3
Sufiyan J T <sufiyan.jt@geekywolf.local>
2025-10-27 17:58:31 +0530

    AssignmentC#15-10-2025Day3

---
+++ C#/Day3_C#Assignment/Day3_C#Assigment/Calculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_C_Assigment
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            int flage = 1;
+            while (flage == 1)
+            {
+                Console.WriteLine("Choose a Operation \n 1.Addition\t\t\n 2.Substation\t\t\n 3.Multipication\t\t\n 4.Divition\t\t\n 5.Exit");
+                String Choice = Console.ReadLine();
+                int Res=0;
+                Console.WriteLine("Enter Two Numbers pls");
+                string StringNumber1 = Console.ReadLine();
+                String StringNumber2 = Console.ReadLine();
+                if (int.TryParse(StringNumber1, out int Number1) && int.TryParse(StringNumber2, out int Number2))
+                {
+                    switch (Choice)
+                    {
+                        case "1":
+                            Res=Add(Number1, Number2);
+                            break;
+                        case "2":
+                            Res = Sub(Number1, Number2);
+                            break;
+                        case "3":
+                            Res = Mul(Number1, Number2);
+                            break;
+                        case "4":
+                            Div(Number1, Number2);
+                            break;
+                        case "5":
+                            flage = 0;
+                            break;
+                        default:
+                            Console.WriteLine("Enter a Valid Numbers");
+                            break;
+
+                    }
+                    Console.WriteLine("Ans= " + Res);
+
+                }
+
+            }
+        }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+
+    }
+}
---
+++ C#/Day3_C#Assignment/Day3_C#Assigment/Program.cs
@@ -0,0 +1,21 @@
+﻿////// See https://aka.ms/new-console-template for more information
+////using Day3_C_Assigment;
+
+//////Console.WriteLine("Hello, World!");
+//////ArithmeticOperations operations = new ArithmeticOperations();
+//////OddOrEvenUsingTernaryOperator ObjOddOrEven = new OddOrEvenUsingTernaryOperator();
+////SwapNumber obj= new SwapNumber();
+//using Day3_C_Assigment;
+
+
+////LieBtween lieBtween = new LieBtween();   
+//Calculator calculator = new Calculator();
+using Day3_C_Assigment;
+
+//Employee employee=new Employee("Sufiyan",10000);
+//BankAccount bankAccount = new BankAccount(1,"Sufiyan",2000.0);
+//bankAccount.Deposit(100);
+//bankAccount.DisplayBalance();
+ //SwapUsingRef swapUsingRef = new SwapUsingRef();
+ 
+//CompareNumbers compareNumbers =new CompareNumbers(10,20);
