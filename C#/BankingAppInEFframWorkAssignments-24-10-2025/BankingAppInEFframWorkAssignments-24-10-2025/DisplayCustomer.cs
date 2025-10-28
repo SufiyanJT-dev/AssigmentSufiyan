0b1a75d4ace9a21fc3de6b826c90592cbf200b8a
Sufiyan J T <sufiyan.jt@geekywolf.local>
2025-10-28 11:05:09 +0530

    Assignment DotNet ef 24-10-2025

---
+++ C#/BankingAppInEFframWorkAssignments-24-10-2025/BankingAppInEFframWorkAssignments-24-10-2025/DisplayCustomer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAppInEFframWorkAssignments_24_10_2025
+{
+    public class DisplayCustomer
+    {
+        public DisplayCustomer()
+        {
+            var contex=new BankingAppDbContext();
+            var customer = new Customer();
+            var customerdisplay = contex.Customer.ToList();
+            foreach (var customers in customerdisplay)
+
+            {               
+                Console.WriteLine("Full name:" + customers.FullName+" Email:"+customers.Email+" Phone Number: "+customers.PhoneNumber+" Date Of Birth: "+customers.DateOfBirth+" Address:"+customers.Address);
+            }
+
+        }
+    }
+}
---
+++ C#/BankingAppInEFframWorkAssignments-24-10-2025/BankingAppInEFframWorkAssignments-24-10-2025/deleteAcc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAppInEFframWorkAssignments_24_10_2025
+{
+    public class deleteAcc
+    {
+        public deleteAcc()
+        {
+            var context = new BankingAppDbContext();
+            var Customers = new Customer();
+            Console.WriteLine("Enter the Id of Customber to be Updated");
+            int Id = Convert.ToInt32(Console.ReadLine());
+            
+
+            var DeleteCustomer = context.Customer.FirstOrDefault(c => c.Id == Id);
+            if (DeleteCustomer != null)
+            {
+               
+                context.Customer.Remove(DeleteCustomer);
+
+                int Deleredeedroe = context.SaveChanges();
+                if (Deleredeedroe > 0)
+                {
+                    Console.WriteLine("Successfully deleted");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Customer does not exist");
+            }
+
+        }
+    }
+}
