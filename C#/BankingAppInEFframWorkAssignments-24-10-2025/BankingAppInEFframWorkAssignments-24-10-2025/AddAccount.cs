0afc6d93464f35f27e2203e18c7d9b8663d46472
Sufiyan J T <sufiyan.jt@geekywolf.local>
2025-10-28 11:13:34 +0530

    Assignment DotNet ef 24-10-2025

---
+++ C#/BankingAppInEFframWorkAssignments-24-10-2025/BankingAppInEFframWorkAssignments-24-10-2025/AddAccount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAppInEFframWorkAssignments_24_10_2025
+{
+    public class AddAccount
+    {
+        public AddAccount()
+        {
+            Console.WriteLine("Enter the Number Of Employees to Be Added");
+            int CountIdis=Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < CountIdis; i++)
+            {
+                Console.WriteLine("Enter the Full Name Of the Employee");
+                string Fullname = Console.ReadLine();
+                Console.WriteLine("Enter the Email Of the Employee");
+                string Email = Console.ReadLine();
+                Console.WriteLine("Enter the Phone NUmber");
+                string PhoneNumber = Console.ReadLine();
+                Console.WriteLine("Enter the Date of Birth in YYYY-MM-DD");
+
+                string stringDateOfBirth = Console.ReadLine();
+                stringDateOfBirth = stringDateOfBirth.Replace('-', ',');
+                if (DateTime.TryParse(stringDateOfBirth, out DateTime dateOfBirth))
+                {
+
+
+                    Console.WriteLine("Enter the Address");
+                    string Address = Console.ReadLine();
+                    DateTime CreatedDate = DateTime.Now;
+
+                    var Contex = new BankingAppDbContext();
+                    Customer customer = new Customer()
+                    {
+                        FullName = Fullname,
+                        Email = Email,
+                        PhoneNumber = PhoneNumber,
+                        DateOfBirth = dateOfBirth,
+                        Address = Address,
+                        CreatedDate = CreatedDate,
+                    };
+                    Contex.Customer.Add(customer);
+                    int rowchange = Contex.SaveChanges();
+                    if (rowchange > 0)
+                    {
+                        Console.WriteLine("successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Check Again");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid date");
+                }
+            }
+        }
+
+    }
+}
+
---
+++ C#/BankingAppInEFframWorkAssignments-24-10-2025/BankingAppInEFframWorkAssignments-24-10-2025/Program.cs
@@ -0,0 +1,51 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//using BankingAppInEFframWorkAssignments_24_10_2025;
+//using Microsoft.EntityFrameworkCore.Metadata.Internal;
+//using System;
+
+//Console.WriteLine("Hello, World!");
+//var context = new BankingAppDbContext();
+//var Customers = new List<Customer>
+//{
+//  new Customer {
+
+//    FullName = "Maria Gonzalez",
+//    Email = "maria.gonzalez@gmail.com",
+//    PhoneNumber = "-1601",
+//    DateOfBirth = new DateTime(1990,08,25),
+//    Address = "45 Calle Mayor, Madrid, Spain",
+//    CreatedDate = new DateTime(2025 - 02 - 15)
+//  },
+
+//  new Customer {
+
+//      FullName = "Liam O’Connor",
+//    Email = "liam.oconnor@outlook.com",
+//    PhoneNumber = "-907779",
+//    DateOfBirth = new DateTime(1985-11-03),
+//    Address = "89 Abbey Rd, London, UK",
+//    CreatedDate = new DateTime(2025-03-10)
+//        },
+//  new Customer{
+//        FullName = "Sophia Müller",
+//    Email = "sophia.mueller@gmail.com",
+//    PhoneNumber = "-2345780",
+//    DateOfBirth = new DateTime(1992-07-18),
+//    Address = "22 Berliner Str, Berlin, Germany",
+//    CreatedDate = new DateTime(2025-04-05)
+//  },
+//  new Customer
+//  {
+//            FullName = "Ethan Brown",
+//    Email = "ethan.brown@yahoo.com",
+//    PhoneNumber = "-1374",
+//    DateOfBirth = new DateTime(1989-02-14),
+//    Address = "17 King St, Sydney, Australia",
+//    CreatedDate = new DateTime(2025-04-05)
+
+//  }
+//    };
+//context.Customer.AddRange(Customers);
+//context.SaveChanges();
+using BankingAppInEFframWorkAssignments_24_10_2025;
+DisplayCustomer displayCustomer = new DisplayCustomer();
